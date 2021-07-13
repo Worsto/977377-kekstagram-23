f406a47a8fd6607fdf4440eac90de7aa03c66d98
Worsto <worsto.inbox@gmail.com>
2021-07-13 18:49:04 +0300

    task1-try2

--- js/api.js
+++ js/api.js
@@ -1,14 +1,13 @@
-import {showAlert} from './utils/show-alert.js';
 
 const LINK_DATA = 'https://23.javascript.pages.academy/kekstagram/data';
 const LINK_FORM = 'https://23.javascript.pages.academy/kekstagram';
 
-const getData = (onSuccess) => {
+const getData = (onSuccess, onError) => {
   fetch(LINK_DATA)
     .then((response) => response.json())
     .then((data) => onSuccess(data))
     .catch(() => {
-      showAlert('Не удалось загрузить данные. Попробуйте перезагрузить страницу');
+      onError('Не удалось загрузить данные. Попробуйте перезагрузить страницу');
     });
 };
 
